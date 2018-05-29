cb647fa86d028b85ff8a9f8d80bdb5ae132ef68b
chechis <jcalvarezg92@gmail.com>
2018-05-28 22:06:45 -0600

    actividad incio
    se van a manejar dos actividades

---
+++ app/src/main/java/jcalv/probando1/inicio/ActivityInicio.java
@@ -0,0 +1,15 @@
+package jcalv.probando1.inicio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import jcalv.probando1.R;
+
+public class ActivityInicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
